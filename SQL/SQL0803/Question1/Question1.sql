9060d456fe26365f6a823168b13c7ec521e0c70b
Bensitu <sito163@163.com>
2022-08-03 17:56:43 +0900

    Exercise 0803

--- SQL/SQL0803/Question1/Question1.sql
+++ SQL/SQL0803/Question1/Question1.sql
@@ -27,10 +27,10 @@ INSERT INTO testResult VALUES(4, '英語', 22);
 SELECT * FROM studentsA WHERE sex='男';
 
 #1教科でも30点以下の点数を取った生徒の名前を一覽で表示する
-SELECT name, score
+SELECT distinct name, score
 FROM studentsA JOIN testResult
 ON studentsA.stu_id = testResult.stu_id
-WHERE score<30;
+WHERE score <= 30;
 
 #性別ごとに、最も高かった試験の点数を表示する
 SELECT sex, MAX(score) 
@@ -41,17 +41,19 @@ GROUP BY sex;
 #教科ごとの試験の平均点が50点以下である教科を表示する
 SELECT course, AVG(score) 
 FROM testResult 
-WHERE score<50 
-GROUP BY course;
+GROUP BY course
+HAVING AVG(score) <= 50;
 
 #試験結果テーブルの点数に右に、その教科の平均点を表示する
-SELECT course, AVG(score) 
+SELECT stu_id, course, score, AVG(score)  
 FROM testResult 
-GROUP BY course;
+GROUP BY stu_id,course, score;
+
+SELECT er1.* (SELECT AVG(testResult.score) FROM textResult WHERE er1.course = er2.course) AS avg_score FROM testResult er1;
 
 #試験結果に理科が含まれない生徒の名前を一覧で表示する
-SELECT distinct name 
-FROM studentsA JOIN testResult
-ON studentsA.stu_id = testResult.stu_id
-WHERE course<>'理科';
+SELECT name
+FROM studentsA
+WHERE stu_id NOT IN (
+SELECT stu_id FROM testResult WHERE course='理科');
 
--- SQL/SQL0803/Question2/Question2.sql
+++ SQL/SQL0803/Question2/Question2.sql
@@ -1,17 +1,17 @@
 #Question2
 
 #ユーザーテーブル
-CREATE TABLE userA(user_id int, user_name VARCHAR(30), email VARCHAR(50), age INT);
+CREATE TABLE userA(user_id Int, user_name VARCHAR(30), email VARCHAR(50), age INT);
 
 #フォローテーブル
-CREATE TABLE followA(follower INT, follow_id int); 
+CREATE TABLE followA(follower INT, follow_id Int); 
 
 #ユーザーテーブルにデータを入力
 INSERT INTO userA VALUES(1, 'もっくん', 'mokkun@example.com', 19);
-INSERT INTO userA VALUES(1, 'みみこ', 'mimiko@example.com', 20);
-INSERT INTO userA VALUES(1, 'さくら', 'sakura@example.com', 31);
-INSERT INTO userA VALUES(1, 'ひよこ', 'hiyoko@example.com', 23);
-INSERT INTO userA VALUES(1, 'すずき', 'suzuki@example.com', 28);
+INSERT INTO userA VALUES(2, 'みみこ', 'mimiko@example.com', 20);
+INSERT INTO userA VALUES(3, 'さくら', 'sakura@example.com', 31);
+INSERT INTO userA VALUES(4, 'ひよこ', 'hiyoko@example.com', 23);
+INSERT INTO userA VALUES(5, 'すずき', 'suzuki@example.com', 28);
 
 #フォローテーブルにデータを入力
 INSERT INTO followA VALUES(1, 2);
@@ -25,3 +25,22 @@ INSERT INTO followA VALUES(5, 1);
 INSERT INTO followA VALUES(5, 2);
 INSERT INTO followA VALUES(5, 3);
 INSERT INTO followA VALUES(5, 4);
+
+#さくらがフォローしているユーザーの名前を一覧で表示する
+SELECT followA.follow_id, userA.user_name
+FROM followA JOIN userA 
+ON userA.user_id = followA.follower AND userA.user_id = followA.follower
+WHERE userA.user_name='さくら';
+
+#誰もフォローしていないユーザーの名前を表示する
+SELECT userA.user_name
+FROM userA JOIN followA
+ON userA.user_id = followA.follow_id AND userA.user_id = followA.follower
+WHERE followA.follower <> null;
+
+#10代、20代、30代といった年代別にフォロー数の平均点を表示する
+
+
+#相互フォローしているユーザーのIDを表示する
+SELECT DISTINCT user_id 
+FROM
